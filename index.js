d31f0b1d8ee92a810f03ed1ec3b1760fdfa3b090
ethswam <423692864@qq.com>
2022-05-26 10:10:54 +0800

    add in  operator

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+let objecOne = {x:1}
 
-console.log("hello")
-console.log("there!")
+let objectTwo = objecOne
+
+console.log(objecOne === objectTwo)
+
+let a  =[1,2,3,4,5]
+
+let c = Array.from(a)
+
+console.log(c)
+
+console.log('7' * '4')
+
+let point = {x:1 , y:1}
+console.log("toString" in point)
